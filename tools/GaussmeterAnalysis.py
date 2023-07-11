cc4bb6f1a1fc43e302be7128159fb2ebe3bc9852
Hans-Bananendans <56868645+Hans-Bananendans@users.noreply.github.com>
2023-07-11 17:36:51 +0200

    OOP conversion for PyQtGraph timeplot
    The one-shot code of 'daytime_magnetic_variation_plot3' has been put in a class structure and has been moved to GaussmeterAnalysis.py. A number of other functionality has been moved to GaussmeterAnalysis, including an EulerRotation class.

--- tools/GaussmeterAnalysis.py
+++ tools/GaussmeterAnalysis.py
@@ -5,6 +5,377 @@ import matplotlib.pyplot as plt
 from matplotlib.gridspec import GridSpec
 from scipy.fft import fft, fftfreq
 from datetime import datetime
+import pyqtgraph as pg
+# from pyqtgraph.Qt import QtCore
+from scipy.signal import savgol_filter
+from copy import deepcopy
+
+
+class TimeplotPyQt:
+    def __init__(self):
+        self.app = pg.mkQApp("Timeplot")
+        self.view = pg.GraphicsView()
+        self.gl = pg.GraphicsLayout()
+
+        self.view.setCentralItem(self.gl)
+
+        # Default settings
+        self.gl.setBorder(100, 100, 100)
+        self.view.setWindowTitle("Timeplot")
+        self.view.resize(800, 600)
+
+        self.gl.layout.setColumnPreferredWidth(0, 25)
+        self.gl.layout.setColumnPreferredWidth(1, 1200)
+
+        pg.setConfigOptions(antialias=True)  # Antialiasing for nicer plots
+
+        self.set_defaults()
+
+
+    def set_defaults(self):
+        self.light_theme = False
+
+        self.padding = [1.1, 1.0]  # Padding factor
+        self.force_identical_scale = True
+        self.pen_alpha = 0.8
+
+        pen_colors = ((255,   0,   0),
+                      (  0, 255,   0),
+                      (  0,  75, 255))
+        self.generate_pens(pen_colors)
+
+        # Colours of the draggable lines, depending on theme
+        if self.light_theme:
+            self.label_fill = (255, 255, 255, 180)  # White label backgrounds
+            self.label_text_color = (100, 100, 100)  # Darker label text
+        else:
+            self.label_fill = (0, 0, 0, 180)
+            self.label_text_color = (200, 200, 100)
+
+        # Background colour:
+        if self.light_theme:
+            self.view.setBackground("white")  # White background
+        else:
+            self.view.setBackground("black")
+
+        self.plot_title = "Plot Title"
+        self.side_label = "Side label"
+
+        self.grids = True
+
+
+    def set_light_theme(self):
+        self.light_theme = True
+
+    def set_dark_theme(self):
+        self.light_theme = False
+
+    def generate_pens(self, pc):
+        self.pen_rgb = pc
+
+        self.pen_rgba = (
+            (pc[0][0], pc[0][1], pc[0][2], self.pen_alpha*100),
+            (pc[1][0], pc[1][1], pc[1][2], self.pen_alpha*100),
+            (pc[2][0], pc[2][1], pc[2][2], self.pen_alpha*100),
+        )
+
+    def set_window_size(self, w: int, h: int):
+        self.view.resize(w, h)
+
+    def set_window_title(self, window_title: str):
+        self.view.setWindowTitle(window_title)
+
+    def set_padding(self, padding_h: float, padding_v: float):
+        self.padding = [padding_h, padding_v]
+
+    def set_force_identical_scale(self, force_identical_scale: bool):
+        self.force_identical_scale = force_identical_scale
+
+    def set_pen_alpha(self, alpha: float):
+        """
+        Set alpha of the pen.
+        Important: set in a range between 0.0 - 1.0 !
+        """
+        self.pen_alpha = alpha
+        generate_pens(self, self.pen_rgb)
+
+    def set_pen_colors(self, pen_colors):
+        """
+        Set colors of the pen.
+        Must be supplied as a list of listed rgb values, e.g.:
+        pen_colors = ((255,   0,   0),
+                      (  0, 255,   0),
+                      (  0,   0, 255))
+        """
+        self.generate_pens(pen_colors)
+
+    def set_label_fill(self, rgba):
+        """
+        Set a label fill RGBA, for example: (255, 255, 255, 180)
+        """
+        self.label_fill = rgba
+
+    def set_label_text_color(self, rgb):
+        """
+        Set a label text RGB, for example: (255, 255, 255)
+        """
+        self.label_text_color = rgb
+
+    def set_background(self, rgb):
+        self.view.setBackground(rgb)
+
+    def set_plot_title(self, plot_title: str):
+        self.plot_title = plot_title
+
+    def set_side_label(self, side_label: str):
+        self.side_label = side_label
+
+    def show_grids(self):
+        self.grids = True
+
+    def hide_grids(self):
+        self.grids = False
+
+    def timeplot_pyqtgraph(self, data: list):
+        # TODO: Plotting stuff
+
+        # # [DEBUG]
+        # for data_list in data:
+        #     print(type(data_list))
+        #     print(type(data_list[0]))
+
+        # Ensure data is of the same length, and of the right type(s)
+        for data_list in (data[1], data[2], data[3]):
+            assert len(data[0]) == len(data_list)
+            assert type(data_list) in (list, np.ndarray)
+            assert type(data_list[0]) in (float, int, np.float64)
+
+        # Unpacking data for plotting
+        # self.data_t = np.array(data[0])
+        # self.data_t_date = []
+        # for i in range(len(data[0])):
+        #     self.data_t_date.append(self.data[0][i])
+        # self.data_x = np.array(data[1])
+        # self.data_y = np.array(data[2])
+        # self.data_z = np.array(data[3])
+
+        self.trange = [data[0][0], data[0][-1]]
+        self.xrange = [min(data[1]), max(data[1])]
+        self.yrange = [min(data[2]), max(data[2])]
+        self.zrange = [min(data[3]), max(data[3])]
+
+        # Force identical scale if set to True:
+        if self.force_identical_scale is True:
+            xmid = self.xrange[0] + (self.xrange[1] - self.xrange[0]) / 2
+            ymid = self.yrange[0] + (self.yrange[1] - self.yrange[0]) / 2
+            zmid = self.zrange[0] + (self.zrange[1] - self.zrange[0]) / 2
+
+            xscale = self.xrange[1] - self.xrange[0]
+            yscale = self.yrange[1] - self.yrange[0]
+            zscale = self.zrange[1] - self.zrange[0]
+
+            scale = max([xscale, yscale, zscale])
+
+            self.xrange = [xmid - scale / 2, xmid + scale / 2]
+            self.yrange = [ymid - scale / 2, ymid + scale / 2]
+            self.zrange = [zmid - scale / 2, zmid + scale / 2]
+
+        # Start constructing plot ============================================
+
+        # Title and side label
+        self.gl.addLabel(self.plot_title, col=1, colspan=2)
+        self.gl.nextRow()
+        self.gl.addLabel(self.side_label, angle=-90, rowspan=3)
+
+        # Data plots
+
+        plotlabels = ("X", "Y", "Z")
+        p_range = (self.xrange, self.yrange, self.zrange)
+        for i, data_array in enumerate((data[1], data[2], data[3])):
+
+            # Main data plot
+            p = self.gl.addPlot(
+                title=plotlabels[i],
+                x=data[0],
+                y=np.array(data_array),
+                pen=pg.mkPen(color=self.pen_rgba[i]),
+                xmin=self.trange[0],
+                xmax=self.trange[1],
+                axisItems={'bottom': pg.DateAxisItem()}
+            )
+            p.setYRange(p_range[i][0], p_range[i][1])
+            p.showGrid(x=self.grids, y=self.grids)
+
+            # Horizontal nfinite line object (min)
+            infline_min = pg.InfiniteLine(
+                angle=0,
+                label="Min: {value:.2f} uT",
+                pen=pg.mkPen(color=self.pen_rgb[i]),
+                pos=p_range[i][0],
+                movable=False,
+                labelOpts={"position": 0.05,
+                           "color": self.pen_rgb[i],
+                           "fill": self.label_fill}
+            )
+            p.addItem(infline_min)
+
+            # Horizontal nfinite line object (max)
+            infline_max = pg.InfiniteLine(
+                angle=0,
+                label="Max: {value:.2f} uT",
+                pen=pg.mkPen(color=self.pen_rgb[i]),
+                pos=p_range[i][1],
+                movable=False,
+                labelOpts={"position": 0.05,
+                           "color": self.pen_rgb[i],
+                           "fill": self.label_fill}
+            )
+            p.addItem(infline_max)
+
+            # # [DEBUG]
+            # print(self.label_text_color)
+            # print(type(self.label_text_color))
+
+            # Horizontal scanline
+            infline_h = pg.InfiniteLine(
+                angle=90,
+                label="{value:.0f}",
+                pos=self.trange[1],
+                movable=True,
+                bounds=[self.trange[0], self.trange[1]],
+                labelOpts={"position": 0.12,
+                           "color": self.label_text_color,
+                           "fill": self.label_fill}
+            )
+            p.addItem(infline_h)
+
+            # Vertical scanline
+            infline_v = pg.InfiniteLine(
+                angle=0,
+                label="{value:.02f}",
+                pos=p_range[i][1],
+                movable=True,
+                bounds=[p_range[i][0], p_range[i][1]],
+                labelOpts={"position": 0.88,
+                           "color": self.label_text_color,
+                           "fill": self.label_fill}
+            )
+            p.addItem(infline_v)
+
+            self.gl.nextRow()
+
+        self.view.show()
+
+        # Executing PyQtGraph
+        pg.exec()
+
+
+class EulerRotation:
+    def __init__(self):
+        pass
+
+    def rx(self, angle):
+        return np.array([[1,             0,              0],
+                         [0, np.cos(angle), -np.sin(angle)],
+                         [0, np.sin(angle),  np.cos(angle)]])
+
+    def ry(self, angle):
+        return np.array([[ np.cos(angle), 0, np.sin(angle)],
+                         [             0, 1,             0],
+                         [-np.sin(angle), 0, np.cos(angle)]])
+
+    def rz(self, angle):
+        return np.array([[np.cos(angle), -np.sin(angle), 0],
+                         [np.sin(angle),  np.cos(angle), 0],
+                         [            0,              0, 1]])
+
+    def rotateX(self, vector, angle, deg=True):
+        if deg:
+            angle *= np.pi / 180
+        return np.dot(self.rx(angle), vector)
+
+    def rotateY(self, vector, angle, deg=True):
+        if deg:
+            angle *= np.pi / 180
+        return np.dot(self.ry(angle), vector)
+
+    def rotateZ(self, vector, angle, deg=True):
+        if deg:
+            angle *= np.pi / 180
+        return np.dot(self.rz(angle), vector)
+
+
+def read_data(filename, header=False, len_header=9):
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+        if lines[0][0:2] == "!H" or header is True:
+            lines = lines[len_header+1:]
+
+        t, x, y, z = [0] * len(lines), [0] * len(lines), [0] * len(lines), [0] * len(lines)
+
+        for i in range(len(lines)):
+            [tt, xx, yy, zz] = lines[i].split(" ")
+            [t[i], x[i], y[i], z[i]] = \
+                [np.double(tt), float(xx), float(yy), float(zz)]
+        return [t, x, y, z]
+
+def vector_normal_distribution(vectors: list):
+    mean_x = 0
+    mean_y = 0
+    mean_z = 0
+
+    sd_x = 0
+    sd_y = 0
+    sd_z = 0
+
+    for vector in vectors:
+        mean_x += vector[0]
+        mean_y += vector[1]
+        mean_z += vector[2]
+
+    mean_x = mean_x / len(vectors)
+    mean_y = mean_y / len(vectors)
+    mean_z = mean_z / len(vectors)
+
+    for vector in vectors:
+        sd_x += (vector[0] - mean_x) ** 2
+        sd_y += (vector[1] - mean_y) ** 2
+        sd_z += (vector[2] - mean_z) ** 2
+
+    sd_x = (sd_x / len(vectors)) ** 0.5
+    sd_y = (sd_y / len(vectors)) ** 0.5
+    sd_z = (sd_z / len(vectors)) ** 0.5
+
+    return [np.array([mean_x, mean_y, mean_z]), np.array([sd_x, sd_y, sd_z])]
+
+def data_savgol_filter(data, windowlength, polyorder=4):
+    # Smooths three data channels using a Savitzky-Golay filter
+    # Data must be in type A
+    x_filtered = savgol_filter(data[1], windowlength, polyorder)
+    y_filtered = savgol_filter(data[2], windowlength, polyorder)
+    z_filtered = savgol_filter(data[3], windowlength, polyorder)
+    return [data[0], x_filtered, y_filtered, z_filtered]
+
+def zipBA(dataB):
+    # Zips data from type B to type A
+    # Type A: [
+    #   [t_0 ... t_n],
+    #   [Bx_0 ... Bx_n],
+    #   [By_0 ... By_n],
+    #   [Bz_0 ... Bz_n],
+    # ]
+    #
+    # Type B: [
+    #   [t_0, np.array(Bx_0, By_0, Bz_0)] ... [t_n, np.array(Bx_n, By_n, Bz_n)]
+    # ]
+    n = len(dataB)
+    dataA = [[0.] * n, [0.] * n, [0.] * n, [0.] * n]
+    for i in range(len(dataB)):
+        dataA[0][i] = dataB[i][0]
+        dataA[1][i] = dataB[i][1][0]
+        dataA[2][i] = dataB[i][1][1]
+        dataA[3][i] = dataB[i][1][2]
+    return dataA
 
 # from GaussmeterLib import (ReadData,
 #                            AnalyzeRate)
@@ -25,7 +396,7 @@ from datetime import datetime
 
 def time_plot(data, ylim=[-100, 100], 
               title="Title",
-              labelx = "X", labely = "Y", labelz = "Z",
+              labelx="X", labely="Y", labelz="Z",
               ):
     fig = plt.figure(figsize=(8, 8))
     gs = GridSpec(1,1)
